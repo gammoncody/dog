f10f801cc3123e065df030c2e2940960b691b66e
gammoncody <60144455+gammoncody@users.noreply.github.com>
2020-02-14 21:25:43 -0600

    Add files via upload

---
+++ dog challenge code.cs
@@ -0,0 +1,27 @@
+using System;
+
+static void Main (string[] agrs);
+
+namespace dog_challenge
+{
+    public enum Gender
+    {
+        Male,
+        Female
+    }
+    public class Dog
+
+    {
+        public string name;
+        public string owner;
+        public int age;
+        public Gender gender;
+        public (string name, string owner, int age, Gender gender)
+        class GetTag; 
+        {
+            public override double GetTag()
+        {
+        return ("If lost, call [owner]. [‘Her’|‘His’] name is [name] and [‘she’|‘he’] is [age]  [‘year’|‘years’] old.");
+        }
+    }    
+}
